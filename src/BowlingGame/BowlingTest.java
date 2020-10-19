bd0131f8d32f346ea49d442bf2ad345fcc5a2709
NoremacSkich <noremacskich@gmail.com>
2020-10-18 23:28:10 -0500

    Got a score function up and running.  Most basic implementation, but gets the job done with a gutter game

--- src/BowlingGame/BowlingTest.java
+++ src/BowlingGame/BowlingTest.java
@@ -3,6 +3,8 @@ package BowlingGame;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BowlingTest {
 
     private Game g;
@@ -15,5 +17,13 @@ public class BowlingTest {
     @Test
     public void canRoll() throws Exception{
         g.roll(0);
+    }
+
+    @Test
+    public void gutterGame() throws Exception{
+        for(int i=0; i<10; i++){
+            g.roll(0);
+        }
+        assertEquals(0, g.score());
     }
 }
